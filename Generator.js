ad87b064d60a0068b85eea4c4048976bc702c312
Pintocuru <flyiruka@gmail.com>
2024-04-07 08:16:06 +0900

    240407

--- Generator.js
+++ Generator.js
@@ -75,6 +75,14 @@ const app = Vue.createApp({
 
 
   methods: {
+    // message、imgが配列でないなら配列にする
+    talk2Array(data) {
+      if (!Array.isArray(data)) {
+        return [{ [data]: 0 }];
+      }
+      return data;
+    },
+
     // CHARACTER がObjectなら配列にする
     Object2Array(data) {
       const images = ["CHARACTER",];
@@ -90,8 +98,8 @@ const app = Vue.createApp({
     },
 
     // ran の数値を合計したものと、引数に対しての％を返す(basicのみ)
-    sumRanValues(num) {
-      const total = this.DATAOBJ.BASIC_IMAGES.reduce((sum, obj) => sum + (obj.hasOwnProperty('ran') ? parseInt(obj.ran) : 0), 0);
+    sumRanValues(num, mode, category = "COMMON") {
+      const total = this.DATAOBJ[category][mode].reduce((sum, obj) => sum + (obj.hasOwnProperty('ran') ? parseInt(obj.ran) : 0), 0);
       return Math.round((num / total) * 100);
     },
     // BASIC/USER:空のデータを追加
@@ -128,26 +136,149 @@ const app = Vue.createApp({
 });
 
 
-app.component('d_titlebar', {
-  props: ['title'],
-  emits: ['addbtn'],
+
+
+// リスト表示＋ダイアログ表示
+app.component('common_v-card', {
+  props: ['color', 'colorname'],
+  data() {
+    return {
+      dialog: false,
+      selectedColor: this.color
+    };
+  },
+  methods: {
+    // OK:親へEmitで送信
+    confirmColor() {
+      this.$emit('color-selected', this.selectedColor);
+      this.dialog = false;
+    },
+    // キャンセル
+    cancelColor() {
+      this.dialog = false;
+    }
+  },
   template: `
-    <v-toolbar density="compact">
-      <v-toolbar-title class="text-h5">{{ title }}</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn variant="elevated" color="primary" @click="$emit('addbtn')">
-        <v-icon>mdi-plus</v-icon> 新規作成
-        <v-tooltip activator="parent" location="bottom">{{ title }} に空のデータを追加します</v-tooltip>
+  <v-dialog v-model="dialog" max-width="500px">
+    <template v-slot:activator="{ on }">
+    <v-row>
+    <v-col>
+     <v-text-field v-model="color" :label="colorname"></v-text-field>
+    </v-col>
+     <v-col cols="auto">
+      <v-btn icon :color="color" v-on:click="dialog = !dialog">
+       <v-icon>mdi-palette</v-icon>
+       <v-tooltip activator="parent" location="bottom">{{colorname}}を変更する</v-tooltip>
       </v-btn>
-    </v-toolbar>
-    <br>
+     </v-col>
+    </v-row>
+    </template>
+    <v-card>
+      <v-card-title>{{colorname}}を変更する</v-card-title>
+      <v-card-text>
+        <v-color-picker show-swatches v-model="selectedColor" />
+      </v-card-text>
+      <v-card-actions>
+      <v-row>
+       <v-col cols="6">
+        <v-btn block variant="flat" color="primary" @click="confirmColor">OK</v-btn>
+      </v-col>
+       <v-col cols="6">
+        <v-btn block @click="cancelColor">Cancel</v-btn>
+      </v-col>
+    </v-row>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+     `
+});
+
+
+
+// リスト表示
+app.component('commoncard', {
+  props: ['select', 'character', 'obj', 'element', 'index',],
+  data() {
+    return {
+    };
+  },
+  methods: {
+    // ran の数値を合計したものと、引数に対しての％を返す
+    sumRanValues(num) {
+      const total = this.obj.reduce((sum, obj) => sum + (obj.hasOwnProperty('ran') ? parseInt(obj.ran) : 0), 0);
+      return Math.round((num / total) * 100);
+    },
+  },
+  template: `
+    <grid_v-card :select="select" :character="character">
+      <v-card-text>
+        <div v-line-clamp="2">{{ element.message }}</div>
+      </v-card-text>
+      <v-btn height="30" width="30" icon @click.stop="clearImageArr('BASIC_IMAGES', element,index)" style="position: absolute; top: 0; right: 0;">
+        <v-icon>mdi-close</v-icon>
+        <v-tooltip activator="parent" location="bottom">この設定を消去</v-tooltip>
+      </v-btn>
+      <v-chip v-show="!element.price" prepend-icon="mdi-chart-pie" color="secondary" variant="flat" style="position: absolute; bottom: 5px; right: 10px;">
+        {{element.ran}} ({{sumRanValues(element.ran)}}%)
+      </v-chip>
+      <v-chip v-show="element.price" prepend-icon="mdi-gift" color="orange" variant="flat" style="position: absolute; bottom: 5px; right: 10px;">
+        {{element.price}} - {{element.priceLimit || ""}}
+      </v-chip>
+      <v-progress-linear :model-value="sumRanValues(element.ran)" color="primary" striped absolute></v-progress-linear>
+    </grid_v-card>
   `,
-})
-
-
+});
 
 
 /*
+
+
+// リスト表示
+app.component('test', {
+  props: ['select', 'character','element','index',],
+  data() {
+    return {
+      dialog: false,
+      pattern: {
+        pattern1: { name: "pattern1", cols: 12, sm: 12, md: 12, lg: 6, height: 50, icon: "mdi-view-list" },
+        pattern2: { name: "pattern2", cols: 12, sm: 6, md: 4, lg: 3, height: 100, icon: "mdi-view-grid" },
+        pattern3: { name: "pattern3", cols: 4, sm: 3, md: 2, lg: 1, height: 100, icon: "mdi-view-module" },
+      },
+    };
+  },
+  computed: {
+    Pattern() {
+      return this.pattern[this.select];
+    },
+    cardStyle() {
+      return {
+        color: this.character['--lcv-text-color'],
+        backgroundColor: this.character['--lcv-background-color']
+      };
+    },
+  },
+  template: `
+    <v-dialog v-model="dialog" max-width="800px">
+     <template v-slot:activator="{ on }">
+      <v-col :cols="Pattern.cols" :sm="Pattern.sm" :md="Pattern.md" :lg="Pattern.lg">
+        <v-card :height="Pattern.height" :style="cardStyle" @click="dialog = !dialog;">
+          <v-toolbar density="compact" color="transparent">
+            <v-toolbar-title class="text-h5">{{element.key}}</v-toolbar-title>
+          </v-toolbar>
+        </v-card>
+      </v-col>
+     </template>
+    <template v-if="dialog">
+      <slot></slot>
+    </template>
+    </v-dialog>
+  `,
+});
+
+
+
+
+
     // 画像ディレクトリを返す
     get_images_dir() {
       const url = new URL(document.URL);
@@ -486,39 +617,58 @@ app.component('d_titlebar', {
   */
 
 
-// リスト表示
-app.component('bannercard', {
-  props: ['value',],
+
+
+
+// カラーテキストとダイアログ表示
+app.component('name_changer_button', {
+  props: ['newtext'],
   data() {
     return {
-      greetings: [
-        { name: 'ユーザーネーム', comment: 'こんにちは！' },
-        { name: 'ユーザーネーム', comment: 'こんばんは！' },
-      ],
-      cardStyle: {
-        color: this.value['--lcv-name-color'], 
-        backgroundColor: this.value['--lcv-background-color'],
-        'border-radius': '20px', 
-      },
-      textcardStyle:{
-        color: this.value['--lcv-text-color']
-      }
+      dialog: false,
+      newText: this.newtext
     };
   },
-  computed: {
-    randomGreeting() {
-      return this.greetings[Math.floor(Math.random() * this.greetings.length)];
+  methods: {
+    confirmChange() {
+      // Emit event with selected text
+      this.$emit('text-changed', this.newText);
+      // Close dialog
+      this.dialog = false;
+    },
+    cancelChange() {
+      // Close dialog without changing text
+      this.dialog = false;
     }
   },
   template: `
-      <v-card :title="randomGreeting.name" height="120" block :style="cardStyle">
-       <v-container>
-        <p :style="textcardStyle">{{ randomGreeting.comment }}</p>
-       </v-container>
-      </v-card>
-      <br>
+    <v-dialog v-model="dialog" max-width="500px">
+      <template v-slot:activator="{ on }">
+        <v-btn icon  v-on:click="dialog = !dialog">
+          <v-icon>mdi-pencil</v-icon>
+          <v-tooltip activator="parent" location="bottom">テキストを変更する</v-tooltip>
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title>テキストを変更する</v-card-title>
+        <v-card-text>
+          <v-text-field v-model="newText" label="新しいテキスト"></v-text-field>
+        </v-card-text>
+      <v-card-actions>
+      <v-row>
+       <v-col cols="6">
+        <v-btn block variant="flat" color="primary" @click="confirmChange">OK</v-btn>
+      </v-col>
+       <v-col cols="6">
+        <v-btn block @click="cancelChange">Cancel</v-btn>
+      </v-col>
+    </v-row>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
      `
 });
+
 
 
 /*
--- Generator_COMP.js
+++ Generator_COMP.js
@@ -6,34 +6,155 @@
 
 // draggable用
 app.component(
- 'draggable', draggable,
+  'draggable', draggable,
 ),
 
 
- // リスト表示
- app.component('grid_v-card', {
-  props: ['select'],
-  emits: ['dialogbtn'],
+  // 各ページのタイトルバー
+  app.component('d_titlebar', {
+    props: ['title'],
+    emits: ['addbtn'],
+    template: `
+  <v-toolbar density="compact">
+    <v-toolbar-title class="text-h5">{{ title }}</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn variant="elevated" color="primary" @click="$emit('addbtn')">
+      <v-icon>mdi-plus</v-icon> 新規作成
+        <v-tooltip activator="parent" location="bottom">{{ title }} に空のデータを追加します</v-tooltip>
+      </v-btn>
+    </v-toolbar>
+    <br>
+`,
+  })
+
+
+
+
+// リスト表示
+app.component('grid_v-card', {
+  props: ['select', 'character'],
+  data() {
+    return {
+      pattern: {
+        pattern1: { name: "pattern1", cols: 12, sm: 12, md: 12, lg: 6, height: 50, icon: "mdi-view-list" },
+        pattern2: { name: "pattern2", cols: 12, sm: 6, md: 4, lg: 3, height: 100, icon: "mdi-view-grid" },
+        pattern3: { name: "pattern3", cols: 4, sm: 3, md: 2, lg: 1, height: 100, icon: "mdi-view-module" },
+      },
+    };
+  },
+  computed: {
+    Pattern() {
+      return this.pattern[this.select];
+    },
+    cardStyle() {
+      return {
+        color: this.character['--lcv-text-color'],
+        backgroundColor: this.character['--lcv-background-color']
+      };
+    },
+  },
   template: `
     <v-col :cols="Pattern.cols" :sm="Pattern.sm" :md="Pattern.md" :lg="Pattern.lg">
-     <v-card :height="Pattern.height" @click="$emit('dialogbtn')">
+     <v-card :height="Pattern.height" :style="cardStyle">
       <slot></slot>
      </v-card>
     </v-col>
   `,
-  computed: {
-   Pattern() {
-    return this[this.select];
-   }
-  },
+})
+
+
+
+
+// フキダシテスト表示
+app.component('bannercard', {
+  props: ['value',],
   data() {
-   return {
-    pattern1: { name: "pattern1", cols: 12, sm: 12, md: 12, lg: 6, height: 50, icon: "mdi-view-list" },
-    pattern2: { name: "pattern2", cols: 12, sm: 6, md: 4, lg: 3, height: 100, icon: "mdi-view-grid" },
-    pattern3: { name: "pattern3", cols: 4, sm: 3, md: 2, lg: 1, height: 100, icon: "mdi-view-module" },
-   };
+    return {
+      greetings: [
+        { name: 'ユーザーネーム', comment: 'こんにちは！' },
+        { name: 'ユーザーネーム', comment: 'こんばんは！' },
+      ],
+      cardStyle: {
+        color: this.value['--lcv-name-color'],
+        backgroundColor: this.value['--lcv-background-color'],
+        'border-radius': '20px',
+      },
+      textcardStyle: {
+        color: this.value['--lcv-text-color']
+      }
+    };
   },
- })
+  computed: {
+    randomGreeting() {
+      return this.greetings[Math.floor(Math.random() * this.greetings.length)];
+    }
+  },
+  template: `
+      <v-card :title="randomGreeting.name" height="120" block :style="cardStyle">
+       <v-container>
+        <p :style="textcardStyle">{{ randomGreeting.comment }}</p>
+       </v-container>
+      </v-card>
+      <br>
+     `
+});
+
+
+
+// カラーテキストとダイアログ表示
+app.component('color_choose', {
+  props: ['color', 'colorname'],
+  data() {
+    return {
+      dialog: false,
+      selectedColor: this.color
+    };
+  },
+  methods: {
+    // OK:親へEmitで送信
+    confirmColor() {
+      this.$emit('color-selected', this.selectedColor);
+      this.dialog = false;
+    },
+    // キャンセル
+    cancelColor() {
+      this.dialog = false;
+    }
+  },
+  template: `
+  <v-dialog v-model="dialog" max-width="500px">
+    <template v-slot:activator="{ on }">
+    <v-row>
+    <v-col>
+     <v-text-field v-model="color" :label="colorname"></v-text-field>
+    </v-col>
+     <v-col cols="auto">
+      <v-btn icon :color="color" v-on:click="dialog = !dialog">
+       <v-icon>mdi-palette</v-icon>
+       <v-tooltip activator="parent" location="bottom">{{colorname}}を変更する</v-tooltip>
+      </v-btn>
+     </v-col>
+    </v-row>
+    </template>
+    <v-card>
+      <v-card-title>{{colorname}}を変更する</v-card-title>
+      <v-card-text>
+        <v-color-picker show-swatches v-model="selectedColor" />
+      </v-card-text>
+      <v-card-actions>
+      <v-row>
+       <v-col cols="6">
+        <v-btn block variant="flat" color="primary" @click="confirmColor">OK</v-btn>
+      </v-col>
+       <v-col cols="6">
+        <v-btn block @click="cancelColor">Cancel</v-btn>
+      </v-col>
+    </v-row>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+     `
+});
 
 
 
